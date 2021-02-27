4062d6218193f343147022e7012f5677c824c8f5
syd333 <sydmiranda@icloud.com>
2021-02-27 12:14:41 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your code here
+const submitData = (name, email) => {
+    const userData = {
+        name: name,
+        email: email
+    }
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+    body: JSON.stringify(userData)
+    })
+    .then(resp => resp.json())
+    .then(json => document.body.innerHTML = json.id)
+    .catch(error => document.body.innerHTML = error.message)
+}
+
